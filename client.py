ea62306e73cd068f001c5b27df01a5c780f0e1c2
atakiel <git@atakiel.com>
2019-11-21 23:20:01 +0200

    fix: handle winner id and other bug fixes

--- client.py
+++ client.py
@@ -32,14 +32,14 @@ class Client():
     self.send("GAME-JOIN")
     self.loop()
 
-  def end_game(self, response):
+  def end_game(self, winner_id):
     self.game_on = False
     self.send("GAME-WON ACK")
     print("----------")
     print("GAME OVER!")
     print("----------")
 
-    if starter_id == self.own_id:
+    if winner_id == self.own_id:
       print("\nYou won!")
     else:
       print("\nYou lose!")
@@ -74,7 +74,7 @@ class Client():
     self.game.opponent_action(row, column)
 
     next_turn_number, row, column = self.game.take_action(next_turn_number)
-    self.send("TURN {} {} {}".format(str(next_turn_number).zfill(3), row + 1, column + 1))
+    self.send("TURN {} {} {}".format(str(next_turn_number).zfill(3), str(row + 1), str(column + 1)))
 
   def print_messages(self):
     print("\n\nMessages:\n\n")
@@ -93,7 +93,7 @@ class Client():
           "GAME-READY": lambda r: self.game_ready(r[1], r[2], r[3]),
           "TURN": lambda r: self.turn(r[1], r[2], r[3]),
           "TURN-ACK": lambda r: self.turn_ack(r[1]),
-          "GAME-WON": lambda r: self.end_game(r),
+          "GAME-WON": lambda r: self.end_game(r[1]),
         }[response[0]](response)
 
     print("Game loop ended")
